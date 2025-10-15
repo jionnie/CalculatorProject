93fadf734932304affd3d4c7da8d3b6a7d8c01c5
jiwon jung <wldnjs2277@gmail.com>
2025-10-15 12:09:15 +0900

    feat: level2 Calculator 필수 기능 구현

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Calculator calc = new Calculator();
 
         while (true) {
             // 양의 정수 2개 입력 받기
@@ -17,39 +18,22 @@ public class App {
             System.out.print("어떤 연산을 하시겠습니까? (+, -, *, /): ");
             char op = sc.next().charAt(0);
 
-            int result = 0;
-            // 사칙연산 기호에 맞는 연산 수행
-            switch (op) {
-                case '+':
-                    result = x + y;
-                    break;
-                case '-':
-                    result = x - y;
-                    break;
-                case '*':
-                    result =  x * y;
-                    break;
-                case '/':
-                    if (y == 0) {
-                        System.out.println("나눗셈 연산에서 분모가 0이 될 수 없습니다.");
-                        continue;
-                    }
-                    result = x / y;
-                    break;
-                default:
-                    System.out.println("잘못된 입력입니다! (+, -, *, /) 중 하나를 입력하세요.");
-                    continue;
-            }
-
-            // 연산 결과 출력
+            // 입력값을 매개값으로 넘기며 연산 수행
+            int result = calc.calculate(x, y, op);
+            
+            // 결과값 출력
             System.out.println("연산 결과: " + result);
 
+            // 결과값 삭제 (값이 저장 되자마자 지워져서 초기 상태가 됨)
+            calc.removeResult();
+
             // exit 입력 시 반복문 종료
-            System.out.print("계산을 계속 하시겠습니까? (exit 입력 시 종료): ");
+            System.out.print("계산을 계속 하시려면 아무 키나 입력하세요. (exit 입력 시 종료): ");
             String exit = sc.next();
             if (exit.equals("exit")) {
                 break;
             }
         }
+
     }
 }
---
+++ src/com/example/calculator/Calculator.java
@@ -0,0 +1,60 @@
+package com.example.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    // 연산 결과를 저장하는 컬렉션 타입 필드
+    private List<Number> results = new ArrayList<Number>();
+
+    // getter
+    public List<Number> getResults() {
+        return results;
+    }
+
+    // setter
+    public void setResults(List<Number> newResults) {
+        this.results = newResults;
+    }
+
+    // 양의 정수 2개와 연산 기호를 매개변수로 받아 사칙연산을 수행하는 메소드
+    public int calculate(int x, int y, char op) {
+
+        // 사칙연산 기호에 맞는 연산 수행
+        switch (op) {
+            case '+':
+                results.add(x + y);
+                break;
+            case '-':
+                results.add(x - y);
+                break;
+            case '*':
+                results.add(x * y);
+                break;
+            case '/':
+                if (y == 0) {
+                    System.out.println("나눗셈 연산에서 분모가 0이 될 수 없습니다.");
+                    break;
+                }
+                results.add(x / y);
+                break;
+            default:
+                System.out.println("잘못된 입력입니다! (+, -, *, /) 중 하나를 입력하세요.");
+        }
+
+        /*
+        Integer에서 int 값 빼내서 반환
+        ArrayList(result)의 size가 0이 아닐 때만 result.size() - 1
+         */
+        if (results.isEmpty()) {
+            return 0;
+        } else {
+            return results.get(results.size() - 1).intValue();
+        }
+    }
+
+    // 저장된 연산 결과들 중 가장 먼저 저장된 데이터를 삭제하는 기능을 가진 메소드
+    public void removeResult() {
+        results.remove(0);
+    }
+}
