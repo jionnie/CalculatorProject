3ae15c6f994117d61601dc23190dc14ce15219b4
jiwon jung <wldnjs2277@gmail.com>
2025-10-16 11:30:27 +0900

    fix: App.java에서 컬렉션이 비어있지 않을 때만 요소를 제거하도록 수정&Calculator.java에서 컬렉션 제네릭 타입을 Number에서 Integer로 수정

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -20,12 +20,14 @@ public class App {
 
             // 입력값을 매개값으로 넘기며 연산 수행
             int result = calc.calculate(x, y, op);
-            
+
             // 결과값 출력
             System.out.println("연산 결과: " + result);
 
             // 결과값 삭제 (값이 저장 되자마자 지워져서 초기 상태가 됨)
-            calc.removeResult();
+            if (!calc.getResults().isEmpty()) {
+                calc.removeResult();
+            }
 
             // exit 입력 시 반복문 종료
             System.out.print("계산을 계속 하시려면 아무 키나 입력하세요. (exit 입력 시 종료): ");
--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -5,16 +5,16 @@ import java.util.List;
 
 public class Calculator {
     // 연산 결과를 저장하는 컬렉션 타입 필드
-    private List<Number> results = new ArrayList<Number>();
+    private List<Integer> results = new ArrayList<Integer>();
 
     // getter
     // 그냥 return results; 하면 외부에서 접근 가능(private 접근 제한이 무의미) -> 원본의 복사본을 반환
-    public List<Number> getResults() {
+    public List<Integer> getResults() {
         return new ArrayList<>(results);
     }
 
     // setter
-    public void setResults(List<Number> newResults) {
+    public void setResults(List<Integer> newResults) {
         this.results = newResults;
     }
 
@@ -42,13 +42,13 @@ public class Calculator {
             default:
                 System.out.println("잘못된 입력입니다! (+, -, *, /) 중 하나를 입력하세요.");
         }
-
-        // Integer에서 int 값 빼내서 반환
+        
+        // 가장 마지막에 추가된 요소 반환
         // ArrayList(result)의 size가 0이 아닐 때만 result.size() - 1
         if (results.isEmpty()) {
             return 0;
         } else {
-            return results.get(results.size() - 1).intValue();
+            return results.get(results.size() - 1);
         }
     }
 
